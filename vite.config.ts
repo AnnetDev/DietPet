20ffb46600a165a950351b18a3820b4d0f2ebb68
Anna <annabaidikova92@gmail.com>
2026-02-10 22:23:36 +0100

    init: vite + react + tailwind + pwa

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  base: './',
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'DietPet',
+        short_name: 'DietPet',
+        description: 'Pet nutrition tracker',
+        theme_color: '#C17A3A',
+        background_color: '#F5EDE0',
+        display: 'standalone',
+        orientation: 'portrait',
+        icons: [
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+})
