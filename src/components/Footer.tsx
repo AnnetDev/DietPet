668bca2dfbda55c7f0d61e8b6af5a7e1a5b05fde
Anna <annabaidikova92@gmail.com>
2026-02-12 14:40:07 +0100

    feat: enhance EditPetModal and Footer styles, update WeightPage date input width

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <div className="border-t border-border bg-card px-6 pb-8 pt-3 flex justify-between items-center">
+        <div className="border-t border-border bg-card px-6 pb-8 pt-3 flex justify-between items-center rounded-tr-xl rounded-tl-xl ">
             <div className="text-base font-black text-primary">
                 Diet<span className="text-accent">Pet</span>
             </div>
