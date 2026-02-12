9754b759f9c74cc04336269a04e51868535e8129
Anna <annabaidikova92@gmail.com>
2026-02-12 12:13:08 +0100

    feat: update base path in Vite configuration for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
-  base: './',
+  base: '/DietPet/',
   plugins: [
     react(),
     svgr(),
@@ -20,12 +20,12 @@ export default defineConfig({
         orientation: 'portrait',
         icons: [
           {
-            src: 'icon-192.png',
+            src: 'icon-192.png?v=2',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'icon-512.png',
+            src: 'icon-512.png?v=2',
             sizes: '512x512',
             type: 'image/png'
           }
